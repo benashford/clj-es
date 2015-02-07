9da354957d7e8d35c82eca3e9017d6eb990feaf2
Ben Ashford <ben@bcash.org>
2015-02-07 19:10:29 +0000

    Initial revision

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-es "0.1.0-SNAPSHOT"
+  :description "Lightweight async client for the ElasticSearch REST API"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [cheshire "5.4.0"]
+                 [http-kit "2.1.18"]])
