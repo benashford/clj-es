8ff2e4b6f97875176a8588a85a7c4856e9c76da7
Ben Ashford <ben@bcash.org>
2015-02-08 12:13:00 +0000

    More functions

--- src/clj_es/client.clj
+++ src/clj_es/client.clj
@@ -1,6 +1,8 @@
 (ns clj-es.client
   (:refer-clojure :exclude [get])
-  (:require [clj-es.core :refer :all]))
+  (:require [clojure.string :as s]
+            [cheshire.core :as json]
+            [clj-es.core :refer :all]))
 
 (def index
   (typical-call
@@ -28,3 +30,63 @@
    (fn [es index-name type id]
      (call-es es :delete (make-url index-name type id)))
    good-or-not-found-status))
+
+(def update
+  (typical-call
+   (fn [es index-name type id body]
+     (call-es es
+              :post (make-url index-name type id "_update")
+              :json-body body))
+   good-or-not-found-status))
+
+(def multi-get
+  (typical-call
+   (fn multi-get*
+     ([es body]
+      (multi-get* es nil nil body))
+     ([es index-name body]
+      (multi-get* es index-name nil body))
+     ([es index-name type body]
+      (call-es es
+               :get (make-url index-name type "_mget")
+               :json-body body)))
+   good-status))
+
+(def bulk
+  (typical-call
+   (fn bulk*
+     ([es ops]
+      (bulk* es nil nil ops))
+     ([es index-name ops]
+      (bulk* es index-name nil ops))
+     ([es index-name type ops]
+      (call-es es
+               :post (make-url index-name type "_bulk")
+               :body (s/join ""
+                             (map #(str (json/encode %) "\n") (flatten ops))))))
+   good-status))
+
+(def delete-by-query
+  (typical-call
+   (fn delete-by-query*
+     ([es query]
+      (delete-by-query* es "_all" query))
+     ([es index-name query]
+      (delete-by-query* es index-name nil query))
+     ([es index-name type query]
+      (let [index-term (if (string? index-name)
+                         index-name
+                         (s/join "," index-name))
+            type-term  (when type
+                         (if (string? type)
+                           type
+                           (s/join "," type)))
+            params     (when (string? query)
+                         {:q query})
+            query      (when-not (string? query) query)]
+        (call-es es
+                 :delete (make-url index-term type-term "_query")
+                 :json-body query
+                 :params params))))
+   good-status))
+
