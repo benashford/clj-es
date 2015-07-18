45c7a07fb90ad03257b237ebc5a958fbd67600ed
Ben Ashford <benashford@users.noreply.github.com>
2015-07-18 21:31:10 +0000

    Version bumps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-es "0.1.0-SNAPSHOT"
   :description "Lightweight async client for the ElasticSearch REST API"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [cheshire "5.4.0"]
-                 [http-kit "2.1.18"]])
+                 [cheshire "5.5.0"]
+                 [http-kit "2.1.19"]])
