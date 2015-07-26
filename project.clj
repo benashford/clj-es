2e9d5ab5784db969e7fdb4e60f55ef63a66292df
Ben Ashford <benashford@users.noreply.github.com>
2015-07-26 16:30:44 +0000

    Applying licence

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject clj-es "0.1.0-SNAPSHOT"
   :description "Lightweight async client for the ElasticSearch REST API"
+  :url "https://github.com/benashford/clj-es"
+  :license {:name         "Apache Licence 2.0"
+            :url          "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cheshire "5.5.0"]
